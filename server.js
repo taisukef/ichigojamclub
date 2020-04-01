b75548adc09ec014b7741dcb6a6229d2a99ab92d
Taisuke Fukuno <fukuno@jig.jp>
2020-04-01 11:09:12 +0900

    support ask!

--- server.js
+++ server.js
@@ -7,8 +7,9 @@ const server = http.createServer()
 server.on('request', function(req, res) {
   console.log(req.url)
   const urlq = url.parse(req.url, true)
-  if (urlq.ask) {
-    const ask = urlq.ask
+  console.log(urlq.query.ask)
+  if (urlq.query.ask) {
+    const ask = urlq.query.ask
     fs.writeFileSync('data/ask/' + new Date().getTime() + ".txt", ask)
     res.writeHead(200, { 'Content-Type' : 'text/plain; charset=ShiftJIS' })
     res.write("' Thank you! Please wait the answer.\n")
