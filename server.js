fdc2e5dc51d2143609f8ada964fbc366d5ee6e63
Taisuke Fukuno <fukuno@jig.jp>
2020-04-01 10:55:56 +0900

    Initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const http = require('http')
+const fs = require('fs')
+const url = require('url')
+const simplewebserver = require('./simplewebservertxt.js')
+
+const server = http.createServer()
+server.on('request', function(req, res) {
+  console.log(req.url)
+  const urlq = url.parse(req.url, true)
+  if (urlq.ask) {
+    const ask = urlq.ask
+    fs.writeFileSync('data/ask/' + new Date().getTime() + ".txt", ask)
+    res.writeHead(200, { 'Content-Type' : 'text/plain; charset=ShiftJIS' })
+    res.write("' Thank you! Please wait the answer.\n")
+    res.write('?"MJ GET ichigojam.club/')
+    res.end()
+  }
+  simplewebserver.serve(res, req.url)
+  res.end()
+})
+server.listen(8001)
